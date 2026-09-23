8a50ce8a875be36f3e9a2bcdb81af112890561c5
Leonid328 <leonidbondari152@gmail.com>
2026-09-23 18:22:59 +0300

    Update lesson 02 leds sketch

--- lesson-02/superloop_leds/superloop_leds.ino
+++ lesson-02/superloop_leds/superloop_leds.ino
@@ -1,51 +1,42 @@
-// Superloop: 3 independent LED tasks in one loop()
-// LED1: 200 ms, LED2: 500 ms, LED3: 1000 ms
-// No delay(), no waiting while() in loop(). Timing via millis().
+int led1 = 8;
+int led2 = 9;
+int led3 = 10;
 
-const int LED1_PIN = 8;
-const int LED2_PIN = 9;
-const int LED3_PIN = 10;
-
-const unsigned long LED1_INTERVAL_MS = 200;
-const unsigned long LED2_INTERVAL_MS = 500;
-const unsigned long LED3_INTERVAL_MS = 1000;
-
-unsigned long led1LastMs = 0;
-unsigned long led2LastMs = 0;
-unsigned long led3LastMs = 0;
-
-bool led1On = false;
-bool led2On = false;
-bool led3On = false;
+unsigned long t1 = 0;
+unsigned long t2 = 0;
+unsigned long t3 = 0;
 
 void setup() {
-  pinMode(LED1_PIN, OUTPUT);
-  pinMode(LED2_PIN, OUTPUT);
-  pinMode(LED3_PIN, OUTPUT);
-
-  digitalWrite(LED1_PIN, LOW);
-  digitalWrite(LED2_PIN, LOW);
-  digitalWrite(LED3_PIN, LOW);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   unsigned long now = millis();
 
-  if (now - led1LastMs >= LED1_INTERVAL_MS) {
-    led1LastMs = now;
-    led1On = !led1On;
-    digitalWrite(LED1_PIN, led1On ? HIGH : LOW);
+  if (now - t1 >= 200) {
+    t1 = now;
+    digitalWrite(led1, !digitalRead(led1));
+    Serial.print(now);
+    Serial.print(" LED1 ");
+    Serial.println(digitalRead(led1));
   }
 
-  if (now - led2LastMs >= LED2_INTERVAL_MS) {
-    led2LastMs = now;
-    led2On = !led2On;
-    digitalWrite(LED2_PIN, led2On ? HIGH : LOW);
+  if (now - t2 >= 500) {
+    t2 = now;
+    digitalWrite(led2, !digitalRead(led2));
+    Serial.print(now);
+    Serial.print(" LED2 ");
+    Serial.println(digitalRead(led2));
   }
 
-  if (now - led3LastMs >= LED3_INTERVAL_MS) {
-    led3LastMs = now;
-    led3On = !led3On;
-    digitalWrite(LED3_PIN, led3On ? HIGH : LOW);
+  if (now - t3 >= 1000) {
+    t3 = now;
+    digitalWrite(led3, !digitalRead(led3));
+    Serial.print(now);
+    Serial.print(" LED3 ");
+    Serial.println(digitalRead(led3));
   }
 }
